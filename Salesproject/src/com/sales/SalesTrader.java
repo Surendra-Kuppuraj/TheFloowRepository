cf0f456d787f7cc3032b94e6987323357ebbb9c0
Surendra-Kuppuraj <surendrakuppuraj@surendras-mbp.local.lan>
2017-07-12 17:08:37 +0530

    initial commit

---
+++ Salesproject/src/com/sales/SalesTrader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sales;
+
+/**
+ * @author surendrakuppuraj
+ *
+ */
+public class SalesTrader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asasasa");
+	}
+
+}
